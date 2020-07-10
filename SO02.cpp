1b770c432486b671c6bab9a88c67948e0c71cb71
leann0101 <68077460+leann0101@users.noreply.github.com>
2020-07-10 22:15:13 +0800

    Update SO02.cpp

--- SO02.cpp
+++ SO02.cpp
@@ -11,24 +11,24 @@ int main(){
   int in;
   string input;
   while(cin >> input){
-    if(input.compare("-a")==0 || input.compare("-d")==0)
+    if(input.compare("-a")==0 || input.compare("-d")==0)//-a -d代表讀完了
       break;
-    in = atoi(input.c_str());
+    in = atoi(input.c_str());//string轉數字
     q.push_back(in);
   }
-  if(input.compare("-a")==0)
+  if(input.compare("-a")==0)//-a的話排小到大
     sort(q.begin(), q.end());
   else
-    sort(q.rbegin(), q.rend());
+    sort(q.rbegin(), q.rend());//-d的話排大到小
   int len = q.size();
   cout << "Array =";
   for (int i = 0; i < len;i++)
-    cout << " " << q[i];
+    cout << " " << q[i];//輸出數字
   cout << endl;
 
-  while(cin >> devide){
+  while(cin >> devide){//讀入要多少個
     cout << "N = " << devide << ", Array =";
-    devide = len / devide;
+    devide = len / devide;//除完找出要幾個
     for (int i = 0; i < devide;i++)
       cout << " " << q[i];
     cout << endl;
