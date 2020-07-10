f358af94cbe6b01350469ff0aa9a5899355015c5
leann0101 <68077460+leann0101@users.noreply.github.com>
2020-07-10 22:02:21 +0800

    Update GM28.cpp

--- GM28.cpp
+++ GM28.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(){
   int n;
   cin >> n;
-  cout << n*(n - 1)*(n - 2)*(n - 3) / 24 << endl;
+  cout << n*(n - 1)*(n - 2)*(n - 3) / 24 << endl;//計算n承上(n - 1)呈上(n - 2)呈上(n - 3) 除以 24
   return 0;
 }
