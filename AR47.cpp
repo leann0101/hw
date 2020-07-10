fb9679d67240b348254689aee14731adfb783c5a
leann0101 <68077460+leann0101@users.noreply.github.com>
2020-07-10 21:36:51 +0800

    Update AR47.cpp

--- AR47.cpp
+++ AR47.cpp
@@ -1,26 +1,17 @@
 #include <iostream>
-#include <string>
 using namespace std;
-     
+
 int main() {
-  string a,b;
-  while(1){
-    cin >> a >> b;
-    if(a=="0" && b=="0")
-      break;
-    int cnt = 0;
-    int n = a.length();
-    if(b.length()>n)
-      n = b.length();
-    for (int t = 0; t < n;t++){
-      cnt += (int(a[t]) + int(b[t]) - 96) / 10;
-    }
-    if(cnt==0)
-      cout << "No carry operation." << endl;
-    else if (cnt==1)
-      cout << "1 carry operation." << endl;
-    else
-      cout << cnt << " carry operations." << endl;
-  }
-  return 0;
+     long long int num;
+     cin >> num;
+     while (num%2 == 0)//可以被2整除
+          num/=2;//除以2
+     while (num%3 == 0)//可以被3整除
+          num/=3;//除以3
+     while (num%5 == 0)//可以被5整除
+          num/=5;//除以5
+     if (num == 1)//除完剩下等於一
+          cout << "Regular number!"<<endl;
+     else cout << "Irregular number!"<<endl;
+     return 0;
 }
