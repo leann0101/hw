ffa3099945377949ef87bd375a3e982bedb21096
leann0101 <68077460+leann0101@users.noreply.github.com>
2020-07-10 22:23:44 +0800

    Update SO24.cpp

--- SO24.cpp
+++ SO24.cpp
@@ -14,13 +14,13 @@ struct man{
 int cmp( const void *a , const void *b ){
   struct man *c = (man *)a;
   struct man *d = (man *)b;
-  if(c->grade != d->grade) return d->grade - c->grade;
-  else return c->no - d->no;
+  if(c->grade != d->grade) return d->grade - c->grade;//先比成績
+  else return c->no - d->no;//再比編號
 }
 int cmp2( const void *a , const void *b ){
   struct man *c = (man *)a;
   struct man *d = (man *)b;
-  return c->no - d->no;
+  return c->no - d->no;//比編號
 }
 
 int main(){
@@ -29,11 +29,11 @@ int main(){
   while(cin >> man[i].no >> man[i].name >> man[i].grade){
     i++;
   }
-  qsort(man, i, sizeof(man[0]), cmp);
+  qsort(man, i, sizeof(man[0]), cmp);//按成績排序
   for (int j = 0; j < i;j++){
-    man[j].rank = j + 1;
+    man[j].rank = j + 1;//再依序給他們名次
   }
-  qsort(man, i, sizeof(man[0]), cmp2);
+  qsort(man, i, sizeof(man[0]), cmp2);//再按編號排回來
   for (int j = 0; j < i;j++){
     cout << man[j].no << " " << man[j].name << " " << man[j].grade << " " << man[j].rank << endl;
   }
