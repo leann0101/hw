e9953c814994b17faf0ffb01d47a35712412f26c
leann0101 <68077460+leann0101@users.noreply.github.com>
2020-07-10 22:22:19 +0800

    Update SO23.cpp

--- SO23.cpp
+++ SO23.cpp
@@ -16,11 +16,11 @@ int main(){
       cin >> p;
       a.push_back(p);
     }
-    sort(a.begin(), a.end());
+    sort(a.begin(), a.end());//排序小到大
 
     int j, k;
     for (k = 0, j=n-1; j>0;k++, j--){
-      cnt += a[k] * j;
+      cnt += a[k] * j;//次數剛好是反過來，出現之後後面幾個就要等他，所以是自己的時間乘以 (總數減掉他是第幾個)
     }
     cout << cnt << endl;
   }
