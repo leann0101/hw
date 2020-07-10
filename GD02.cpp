14c553f1d0b5215de507427994ba42e1051fcdbb
leann0101 <68077460+leann0101@users.noreply.github.com>
2020-07-10 21:48:33 +0800

    Update GD02.cpp

--- GD02.cpp
+++ GD02.cpp
@@ -19,13 +19,13 @@ int main(){
       cin >> s2;
       student.push_back(s2);
     }
-    sort(weight.begin(), weight.end());
-    sort(student.begin(), student.end());
+    sort(weight.begin(), weight.end());//排序承重才能依序比較
+    sort(student.begin(), student.end());//排序學生reward才能依序比較
     for (int k = 0; k < weight.size();k++){
       for (int h = 0; h < student.size();h++){
-        if(weight[k] <= student[h]){
-          cnt += student[h];
-          weight.erase(weight.begin()+k);
+        if(weight[k] <= student[h]){//如果這個學生負荷的暸這個重量
+          cnt += student[h];//有排到的就加到cnt，是老師得負的reward
+          weight.erase(weight.begin()+k);//就互相抵消
           student.erase(student.begin()+h);
           k--;
           h--;
@@ -33,10 +33,10 @@ int main(){
         }
       }
     }
-    if (!weight.empty())
+    if (!weight.empty())//如果還有多的重量沒被扛
       cout << "-1" << endl;
     else
-      cout << cnt << endl;
+      cout << cnt << endl;//老師要付多少reward
     cnt = 0;
     vector<int>().swap(weight);
     vector<int>().swap(student);
