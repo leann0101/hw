ca3621ee9decc45124757624a68c1bed008bc374
leann0101 <68077460+leann0101@users.noreply.github.com>
2020-07-10 20:52:09 +0800

    Update AR12.cpp

--- AR12.cpp
+++ AR12.cpp
@@ -9,7 +9,7 @@ struct In {
   int num=0; 
 }In[51];
 
-int cmp(const void *p1,const void *p2) { 
+int cmp(const void *p1,const void *p2) { //qsort 用在對struct排序
   const struct In *pf1 = (const struct In*) p1; 
   const struct In *pf2 = (const struct In*) p2; 
   return (int)(pf2->ans - pf1->ans); 
@@ -21,21 +21,21 @@ int main(){
   for(i = 0; i <= 50; i++){
     cin >> a;
     test = 0;
-    if(a == -999)
+    if(a == -999)//輸入是-999時停止
       break;
     for(j = 0; j <= 50; j++){
-      if(a == In[j].ans){
+      if(a == In[j].ans){//如果輸入的數字之前有輸入過就num+1
         In[j].num++;
         test = 1;
       }
     }
-    if(test == 0){
+    if(test == 0){//如果輸入的數字之前沒有輸入過就新增
       In[cnt].ans = a;
       In[cnt].num = 1;
       cnt++;
     }
   }
-  qsort(In,50,sizeof(In[0]),cmp);
+  qsort(In,50,sizeof(In[0]),cmp);//由大至小排序
   for(j= 0; j < 50; j++){
     if(In[j].num!=0){
       cout << In[j].ans << " " << In[j].num << endl;
