5662c895a23f10c4f8b92ab42a180e229be399f3
leann0101 <68077460+leann0101@users.noreply.github.com>
2020-07-10 21:39:36 +0800

    Update DP10.cpp

--- DP10.cpp
+++ DP10.cpp
@@ -3,9 +3,9 @@
 #include<string>
 using namespace std;
 int f(int n){
-  if( n==1 or n==2 )
+  if( n==1 or n==2 )//n是一或二就回傳1
     return 1;
-  if( n >= 3 )
+  if( n >= 3 )//不然就呼叫自己(f)回傳f(n-1)加上f(n-2)，直到n剩下1或2
     return f(n-1)+f(n-2);
 }
 int main(){
@@ -13,7 +13,7 @@ int main(){
   cin >> all;
   for(int i =0;i<all;i++){
     cin >> n;
-    if(n>=1 && n<=45)
+    if(n>=1 && n<=45)//輸入資料範圍是1-45
       cout << f(n) << endl;
   }
   return 0;
