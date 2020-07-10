3edc5b77108c35d97bf6edb82c85c4ff4abf150d
leann0101 <68077460+leann0101@users.noreply.github.com>
2020-07-10 22:06:49 +0800

    Update MM133.cpp

--- MM133.cpp
+++ MM133.cpp
@@ -7,16 +7,16 @@ int main(){
   long double cnt = 0;
   int n;
   cin >> n;
-  if(n%2==0){
+  if(n%2==0){//偶數的話
     for (long double i = 2; i <= n;i=i+2){
-      cnt += 1 / i;
+      cnt += 1 / i;//算1/2 1/4...
     }
   }
-  else {
+  else {//奇數的話
     for (long double i = 1; i <= n;i=i+2){
-      cnt += 1 / i;
+      cnt += 1 / i;//算1/1 1/3...
     }
   }
-  cout << fixed  <<  setprecision(11) << cnt << endl;
+  cout << fixed  <<  setprecision(11) << cnt << endl;//精準度至小數點下11位
   return 0;
 }
