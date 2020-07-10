3f8e0c125459bc9b48ef02f84eaa2a232a6c017f
leann0101 <68077460+leann0101@users.noreply.github.com>
2020-07-10 22:12:08 +0800

    Update SL12.cpp

--- SL12.cpp
+++ SL12.cpp
@@ -9,16 +9,16 @@ int main(){
     while(1){
       if(word[i]=='\0')
         break;
-      else if(int(word[i])>=48 && int(word[i])<57){
-        cnt = int(word[i]) + 1;
-        word[i] = char(cnt);
+      else if(int(word[i])>=48 && int(word[i])<57){//0~8
+        cnt = int(word[i]) + 1;//加一
+        word[i] = char(cnt);//轉回char
       }
-      else if(int(word[i])==57){
+      else if(int(word[i])==57){//9變成0
         word[i] = '0';
       }
-      else if((int(word[i]) > 64 && int(word[i]) < 91)||(int(word[i]) > 96 && int(word[i]) < 123)){
-        cnt = 187-int(word[i]);
-        word[i] = char(cnt);
+      else if((int(word[i]) > 64 && int(word[i]) < 91)||(int(word[i]) > 96 && int(word[i]) < 123)){//是英文
+        cnt = 187-int(word[i]);// Z TURN TO a，A turn to z，z turn to A，a turn to Z
+        word[i] = char(cnt);//轉回char
       }
       i++;
     }
