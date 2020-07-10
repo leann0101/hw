dbc5869d34829c1466ae10d436ee733d4b6c9931
leann0101 <68077460+leann0101@users.noreply.github.com>
2020-07-10 22:07:31 +0800

    Update MM137.cpp

--- MM137.cpp
+++ MM137.cpp
@@ -7,11 +7,11 @@ int main(){
   int n, q, cnt=2;
   cin >> n;
   for (int i = 0; i < n;i++){
-    cin >> q;
+    cin >> q;//看輸入是幾
     for (int j = 0; j < q-1;j++)
-      cnt = cnt * 2;
+      cnt = cnt * 2;//承以幾次2
     cout << cnt << endl;
-    cnt = 2;
+    cnt = 2;//初始化cnt為2
   }
   return 0;
 }
