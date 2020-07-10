da6a91f1b1cc24fab900b660a6c4d56a17b54cad
leann0101 <68077460+leann0101@users.noreply.github.com>
2020-07-10 22:18:53 +0800

    Update SO22.cpp

--- SO22.cpp
+++ SO22.cpp
@@ -13,13 +13,13 @@ int main(){
     vector<int> a;
     for(int j=0;j<n;j++){
       cin >> p;
-      a.push_back(p);
+      a.push_back(p);//讀入
     }
-    sort(a.begin(), a.end());
-    for (int k = 0; k < a.size();k++)
+    sort(a.begin(), a.end());//排序
+    for (int k = 0; k < a.size();k++)//正著印出來
       cout << a[k];
     cout << endl;
-    for (int k = a.size()-1; k >= 0;k--)
+    for (int k = a.size()-1; k >= 0;k--)//反著印出來
       cout << a[k];
     cout << endl;
   }
