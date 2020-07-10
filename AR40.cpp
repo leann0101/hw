c786be6f62db390c8df0f40f5661dc07e127ce27
leann0101 <68077460+leann0101@users.noreply.github.com>
2020-07-10 21:30:34 +0800

    Update AR40.cpp

--- AR40.cpp
+++ AR40.cpp
@@ -9,15 +9,15 @@ int main(){
   for (int i = 0; i < num;i++){
     cin >> n[i];
   }
-  if(n[0]<=5)
-    cnt += n[0] * 36;
+  if(n[0]<=5)//從右半邊開始
+    cnt += n[0] * 36;//360度除以10，每格是36度，算往右邊跑幾格
   else
-    cnt += (10-n[0])*36;
+    cnt += (10-n[0])*36;//算往左邊跑幾格
   while(j<num-1){
-    if(abs(n[j + 1] - n[j]) <= 5)
-      cnt += abs(n[j + 1] - n[j])*36;
+    if(abs(n[j + 1] - n[j]) <= 5)//兩個相距在5格以內的話
+      cnt += abs(n[j + 1] - n[j])*36;//直接相減
     else
-      cnt += (10 - (abs(n[j + 1] - n[j])))*36;
+      cnt += (10 - (abs(n[j + 1] - n[j])))*36;//不然就從另一個方向跑，用10去減
     j++;
   }
   cout << "Minimal summation of swinging degrees is " << cnt << endl;
