a1aa19e800fe7d344a7b1b6956ce20f57c4e2422
leann0101 <68077460+leann0101@users.noreply.github.com>
2020-07-10 22:01:04 +0800

    Update GM14.cpp

--- GM14.cpp
+++ GM14.cpp
@@ -14,12 +14,12 @@ int main(){
     for (k = 0;k<n-1;k++){
       for (l =1;l<n;l++){
         if(k!=l){
-          if(pow(x[l]-x[k],2)+pow(y[l]-y[k],2)<min)
-            min = pow(x[l] - x[k], 2) + pow(y[l] - y[k], 2);
+          if(pow(x[l]-x[k],2)+pow(y[l]-y[k],2)<min)//如果距離比較小
+            min = pow(x[l] - x[k], 2) + pow(y[l] - y[k], 2);//就把距離存到min
         }
       }
     }
-    cout << min << endl;
+    cout << min << endl;//找出最小距離
     min = 10000;
   }
 }
