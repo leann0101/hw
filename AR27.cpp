bba32dbd790057dedc6cfc06680958c1664861d1
leann0101 <68077460+leann0101@users.noreply.github.com>
2020-07-10 21:23:15 +0800

    Update AR27.cpp

--- AR27.cpp
+++ AR27.cpp
@@ -9,10 +9,11 @@ int main(){
   cin >> num;
   for (int i = 1; i < num;i++){ 
     cin >> ans;
-    a[ans] = ans;
+    a[ans] = ans;//陣列第幾個位子就存對應數字(a[1]存1)
   } 
   for (int i = 1; i <= num;i++){
-    if(a[i] == 0)
+    if(a[i] == 0)//看哪個位子沒有數字
       cout << i << endl;
   }
+ return 0;
 }  
