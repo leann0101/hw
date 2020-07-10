502433ed34463302e9b38115beda233747517aac
leann0101 <68077460+leann0101@users.noreply.github.com>
2020-07-10 21:17:32 +0800

    Update AR23.cpp

--- AR23.cpp
+++ AR23.cpp
@@ -6,15 +6,15 @@ int main(){
   int i = 0;
   cin.getline(c, 1000);
   while(c[i] != '\0'){
-    if( int(c[i]) >= 65 && int(c[i]) <= 77 )
-      c[i] = char(int(c[i]+13));
-    else if( int(c[i]) >= 78 && int(c[i]) <= 90 )
-      c[i] = char(int(c[i]-13));
-    else if( int(c[i]) >= 97 && int(c[i]) <= 109 )
-      c[i] = char(int(c[i]+13));
-    else if( int(c[i]) >= 110 && int(c[i]) <= 122 )
-      c[i] = char(int(c[i]-13));
-    i++;
+    if( int(c[i]) >= 65 && int(c[i]) <= 77 )//如果是A~M
+      c[i] = char(int(c[i]+13));//透過ASCII碼轉成N~Y
+    else if( int(c[i]) >= 78 && int(c[i]) <= 90 )//如果是N~Z
+      c[i] = char(int(c[i]-13));//透過ASCII碼轉成A~M
+    else if( int(c[i]) >= 97 && int(c[i]) <= 109 )//如果是a~m
+      c[i] = char(int(c[i]+13));//透過ASCII碼轉成n~y
+    else if( int(c[i]) >= 110 && int(c[i]) <= 122 )//如果是n~z
+      c[i] = char(int(c[i]-13));//透過ASCII碼轉成a~m
+    i++;//下一個字
   }
   cout << c << endl;
 }
