4638fa23b462816da5c8bd5d5a6a0f395ce5df8d
leann0101 <68077460+leann0101@users.noreply.github.com>
2020-07-10 21:55:40 +0800

    Update GM10.cpp

--- GM10.cpp
+++ GM10.cpp
@@ -8,13 +8,13 @@ int main(){
   for (int i = 0; i < num;i++){
     cin >> x1 >> y1 >> x2 >> y2;
     if(x1 * x2 < 0 && y1 * y2 < 0)
-      cout << "Both" << endl;
+      cout << "Both" << endl;//x跟y座標都是一正一負代表跨過兩個軸
     else if(x1 * x2 > 0 && y1 * y2 > 0)
-      cout << "None" << endl;
+      cout << "None" << endl;//x跟y座標都是同象限代表沒跨過兩個軸
     else if(x1 * x2 < 0 && y1 * y2 > 0)
-      cout << "Y" << endl;
+      cout << "Y" << endl;//x座標一正一負代表跨過Y軸
     else if(x1 * x2 > 0 && y1 * y2 < 0)
-      cout << "X" << endl;
+      cout << "X" << endl;//y座標一正一負代表跨過x軸
   }
   return 0;
 }
