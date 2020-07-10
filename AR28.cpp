4ead53dfff45c7d34a1ff76cbd9011ac054b8358
leann0101 <68077460+leann0101@users.noreply.github.com>
2020-07-10 21:25:25 +0800

    Update AR28.cpp

--- AR28.cpp
+++ AR28.cpp
@@ -8,15 +8,15 @@ int main()
     int* arr = new int[n];
     for (int i = 0; i < n; ++i)
         cin >> arr[i];
-    for (int i = 0; i < n; ++i)
+    for (int i = 0; i < n; ++i)//從每個點
     {
-        for (int j = (i+1); j < n ; ++j)
+        for (int j = (i+1); j < n ; ++j)//去跟其他點比比看
         {
-            if (arr[i] > (2*arr[j]))
-                ++num;
+            if (arr[i] > (2*arr[j]))//如果他比別人的兩倍還大
+                ++num;//num就加一
         }
     }
-    cout << num << endl;
+    cout << num << endl;//找有幾個人比別人的兩倍大
     delete[] arr;
     return 0;
 }
