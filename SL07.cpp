d7476a86cc52c0977315690b3358994762a7b86c
leann0101 <68077460+leann0101@users.noreply.github.com>
2020-07-10 22:09:06 +0800

    Update SL07.cpp

--- SL07.cpp
+++ SL07.cpp
@@ -6,17 +6,17 @@ int main(){
   while(1){
     cin >> word;
     int i = 0, cnt = 0;
-    if(strcmp(word,"end")==0)
+    if(strcmp(word,"end")==0)//輸入end結束
       break;
     while(1){
-      if(word[i]=='\0')
+      if(word[i]=='\0')//讀到結尾就break
         break;
       else if(word[i] == 'a' ||word[i] == 'e'||word[i] == 'i'||word[i] == 'o'||word[i] == 'u'||word[i] == 'A'||word[i] == 'E'||word[i] == 'I'||word[i] == 'O'||word[i] == 'U')
-        cnt+=5;
+        cnt+=5;//是母音加五分
       else
-        cnt += 2;
-      if(int(word[i]) > 64 && int(word[i]) < 91)
-        cnt += 1;
+        cnt += 2;//子音加兩分
+      if(int(word[i]) > 64 && int(word[i]) < 91)//大寫透過判別ASCII
+        cnt += 1;//是大寫額外加一分
       i++;
     }
     cout << cnt << endl;
